c8b77216d0f364582ad0974fa8aeae9335caf08f
khouloud <abdelmalek.khouloud@esprit.tn>
2024-12-16 02:15:33 +0100

    initial commit3

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,28 +1,26 @@
-// Classe Zoo
 public class Zoo {
     Animal[] animals;
     String name;
     String city;
-    int nbrCages;
+    final int MAX_CAGES = 25; // Nombre maximal de cages
+    int animalCount = 0; // Compteur d'animaux
 
-    // Constructeur paramétré pour initialiser le zoo
-    public Zoo(String name, String city, int nbrCages) {
-        animals = new Animal[nbrCages];
+    // Constructeur paramétré
+    public Zoo(String name, String city) {
+        animals = new Animal[MAX_CAGES]; // Limité à 25 animaux
         this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
     }
 
-    // Méthode displayZoo pour afficher les informations du zoo
-    public void displayZoo() {
-        System.out.println("Nom du zoo: " + name);
-        System.out.println("Ville: " + city);
-        System.out.println("Nombre de cages: " + nbrCages);
-    }
-
-    // Méthode toString pour afficher les informations du zoo
-    @Override
-    public String toString() {
-        return "Zoo: " + name + " | Ville: " + city + " | Nombre de cages: " + nbrCages;
+    // Méthode pour ajouter un animal
+    public boolean addAnimal(Animal animal) {
+        if (animalCount < MAX_CAGES) {
+            animals[animalCount] = animal;
+            animalCount++;
+            return true;
+        } else {
+            System.out.println("Le zoo est plein. Impossible d'ajouter l'animal.");
+            return false;
+        }
     }
 }
